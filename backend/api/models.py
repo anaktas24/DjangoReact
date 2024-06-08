d813b519380d62b7660e32113793c4923ae895bd
Ioannis Antoniou <johnantony24@hotmail.com>
2024-06-08 22:36:43 +0200

    task model

--- backend/api/models.py
+++ backend/api/models.py
@@ -1,1 +1,13 @@
 from django.db import models
+from django.contrib.auth.models import User
+
+
+class Task(models.Model):
+    title = models.CharField(max_length=200)
+    active = models.BooleanField(default=True)
+    upcoming = models.BooleanField(default=False)
+    completed = models.BooleanField(default=False)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.title
--- backend/api/serializers.py
+++ backend/api/serializers.py
@@ -1,6 +1,6 @@
 from django.contrib.auth.models import User
 from rest_framework import serializers
-from .models import Note
+from .models import Task
 
 
 class UserSerializer(serializers.ModelSerializer):
@@ -13,3 +13,9 @@ class UserSerializer(serializers.ModelSerializer):
         print(validated_data)
         user = User.objects.create_user(**validated_data)
         return user
+
+class TaskSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Task
+        fields = ["id", "title", "completed", "upcoming","active", "user"]
+        extra_kwargs = {"user": {"read_only": True}}
