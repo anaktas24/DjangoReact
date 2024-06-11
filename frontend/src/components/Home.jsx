082d3486e6809033fee1189d80ab87c069a9f910
Ioannis Antoniou <johnantony24@hotmail.com>
2024-06-12 00:23:02 +0200

    logo

--- frontend/src/components/Home.jsx
+++ frontend/src/components/Home.jsx
@@ -1,21 +1,26 @@
 import React from 'react';
 import '../styles/Home.css';
-import '../assets/react.svg'
+import mango from '../assets/mango.png'
 
-function Home() {
+
+
+const Home = () => {
   return (
     <div className="home">
-      <div className="home-left">
-        <header className="home-header">
-          <h1>Welcome to My Homepage</h1>
-        </header>
-        <section className="home-content">
-          <p>This is a simple home page created with React. Here you can add more information about your website.</p>
-        </section>
-      </div>
-      <div className="home-right">
-        <img src='../assets/react.svg' alt="Description of image" className="home-image" />
-        {/* Alternatively, if you have an animation, you can include it here */}
+      <div className="home-content">
+        <div className="home-left">
+          <button className="cta-button">Click Here</button>
+          <header className="home-header">
+            <h1>The World Is Open To You</h1>
+          </header>
+          <section className="home-description">
+            <h3>Subtitle And Others</h3>
+            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
+          </section>
+        </div>
+        <div className="home-right">
+          <img src={mango} alt="Description of image" className="home-image" />
+        </div>
       </div>
     </div>
   );
--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -5,7 +5,7 @@ const Navbar = () => {
   return (
     <nav className="navbar">
       <div className="navbar-logo">
-        <h1>MyWebsite</h1>
+        <h1>Logo</h1>
       </div>
       <ul className="navbar-links">
         <li><a href="#home">Home</a></li>
