c6ad040db17fc294335b0f80ab18109aff2d5bc4
Ioannis Antoniou <johnantony24@hotmail.com>
2024-06-14 21:56:07 +0200

    links

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -8,12 +8,11 @@ const Navbar = () => {
         <h1>Logo</h1>
       </div>
       <ul className="navbar-links">
-        <li><a href="#home">Home</a></li>
-        <li><a href="#about">About</a></li>
-        <li><a href="#contact">Contact</a></li>
+        <li><a href="home">Home</a></li>
+        <li><a href="about">{<About/>}</a></li>
       </ul>
       <div className="navbar-profile">
-        <button>Profile</button>
+        <li><a href="profile">Profile</a></li>
       </div>
     </nav>
   );
