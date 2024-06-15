4f0071b60219f4c89773615747c8ad03aca7077e
Ioannis Antoniou <johnantony24@hotmail.com>
2024-06-15 22:07:57 +0200

    404

--- frontend/src/components/NotFound.jsx
+++ frontend/src/components/NotFound.jsx
@@ -3,31 +3,31 @@ import '../styles/NotFound.css';
 
 function NotFound() {
   return (
-      <div class="error">
-        <div class="wrap">
-          <div class="404">
+      <div className="error">
+        <div className="wrap">
+          <div className="404">
             <pre><code>
-            <span class="green">&lt;!</span><span>DOCTYPE html</span><span class="green">&gt;</span>
-            <span class="orange">&lt;html&gt;</span>
-            <span class="orange">&lt;style&gt;</span>
+            <span className="green">&lt;!</span><span>DOCTYPE html</span><span className="green">&gt;</span>
+            <span className="orange">&lt;html&gt;</span>
+            <span className="orange">&lt;style&gt;</span>
           * {
         }
-            <span class="orange">&lt;/style&gt;</span>
-            <span class="orange">&lt;body&gt;</span>
+            <span className="orange">&lt;/style&gt;</span>
+            <span className="orange">&lt;body&gt;</span>
                       ERROR 404!
                 FILE NOT FOUND!
-            <span class="comment">&lt;!--The file you are looking for,
-                  is not where you think it is.--&gt;
+            <span className="comment">&lt;!-- ERROR 404!
+            FILE NOT FOUND!--&gt;
             </span>
 
             <br />
-            <span class="info"></span>
+            <span className="info"></span>
               <br />
 
-              <span class="orange">&nbsp;&lt;/body&gt;</span>
+              <span className="orange">&nbsp;&lt;/body&gt;</span>
 
               <br/>
-              <span class="orange">&lt;/html&gt;</span>
+              <span className="orange">&lt;/html&gt;</span>
               </code></pre>
          </div>
         </div>
