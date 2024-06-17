a4bf32ff1566694bbcd2a0035c5f654c6f487a3a
Ioannis Antoniou <johnantony24@hotmail.com>
2024-06-17 22:47:53 +0200

    nav

--- frontend/src/components/Home.jsx
+++ frontend/src/components/Home.jsx
@@ -21,6 +21,7 @@ const Home = () => {
             </p>
 
             <p>`Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.`</p>
+νπμ ρθν δεω
 
           </section>
         </div>
