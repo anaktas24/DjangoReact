dd788edb8f368bda2f24baecb5e11e6aa298881a
Ioannis Antoniou <johnantony24@hotmail.com>
2024-06-17 23:21:55 +0200

    nav

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -1,19 +1,16 @@
 import React from 'react';
 import '../styles/Navbar.css';
+import vite from '../assets/vite.svg';
 
 const Navbar = () => {
   return (
     <nav className="navbar">
-      <div className="navbar-logo">
-        <h1>Logo</h1>
-      </div>
+      <div className="navbar-logo" style = {{backgroundImage: `url(${vite})`}}></div>
       <ul className="navbar-links">
         <li><a href="home">Home</a></li>
         <li><a href="about"></a></li>
+         <li><a href="profile">Profile</a></li>
       </ul>
-      <div className="navbar-profile">
-        <li><a href="profile">Profile</a></li>
-      </div>
     </nav>
   );
 };
