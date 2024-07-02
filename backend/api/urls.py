e3343a529fa96658c47fd74e96a57934c15fce21
Ioannis Antoniou <johnantony24@hotmail.com>
2024-07-02 22:33:03 +0200

    swagger is wokring

--- backend/api/urls.py
+++ backend/api/urls.py
@@ -1,15 +1,25 @@
 # Description: This file contains the URL patterns for the API app.
 
 from django.urls import path
-from django.conf.urls import url
 from . import views
 from rest_framework import permissions
-from rest_framework_swagger.views import get_swagger_view
+from drf_yasg.views import get_schema_view
 
-schema_view = get_swagger_view(title='Pastebin API')
+from drf_yasg import openapi
+
+schema_view = get_schema_view(
+    openapi.Info(
+        title="Django Sample Application API",
+        default_version='v1',
+        description="Welcome to the Django Sample Application API documentation",
+    ),
+    public=True,
+    permission_classes=(permissions.AllowAny,),
+)
 
 urlpatterns = [
     path("user/register/", views.CreateUserView.as_view(), name="register"),
     path('profile/<int:pk>/', views.ProfileDetailView.as_view(), name='profile'),
-    url(r'^$', schema_view)
+    path('swagger/', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
+
    ]
--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -60,6 +60,7 @@ INSTALLED_APPS = [
     "api",
     'drf_yasg',
 
+
 ]
 AUTH_USER_MODEL = "api.User"
 
@@ -94,7 +95,24 @@ TEMPLATES = [
 
 WSGI_APPLICATION = "backend.wsgi.application"
 
-
+SWAGGER_SETTINGS = {
+    'USE_SESSION_AUTH': False,
+    'DEFAULT_FIELD_INSPECTORS': [
+        'drf_yasg.inspectors.CamelCaseJSONFilter',
+        'drf_yasg.inspectors.InlineSerializerInspector',
+        'drf_yasg.inspectors.RelatedFieldInspector',
+        'drf_yasg.inspectors.ChoiceFieldInspector',
+        'drf_yasg.inspectors.FileFieldInspector',
+        'drf_yasg.inspectors.DictFieldInspector',
+        'drf_yasg.inspectors.SimpleFieldInspector',
+        'drf_yasg.inspectors.StringDefaultFieldInspector',
+    ],
+    'SECURITY_DEFINITIONS': {
+        'Basic': {
+            'type': 'basic'
+        }
+    }
+ }
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
