7d6ebc16f3998f335785088afaac699dc0fa2e57
Ioannis Antoniou <johnantony24@hotmail.com>
2024-07-02 22:12:58 +0200

    added swagger new

--- backend/api/urls.py
+++ backend/api/urls.py
@@ -1,28 +1,15 @@
 # Description: This file contains the URL patterns for the API app.
 
 from django.urls import path
+from django.conf.urls import url
 from . import views
 from rest_framework import permissions
-from drf_yasg.views import get_schema_view
-from drf_yasg import openapi
+from rest_framework_swagger.views import get_swagger_view
 
-
-schema_view = get_schema_view(
-    openapi.Info(
-        title="Your API",
-        default_version='v1',
-        description="API documentation",
-        terms_of_service="https://www.example.com/terms/",
-        contact=openapi.Contact(email="contact@example.com"),
-        license=openapi.License(name="BSD License"),
-    ),
-    public=True,
-    permission_classes=(permissions.AllowAny,),
-)
+schema_view = get_swagger_view(title='Pastebin API')
 
 urlpatterns = [
     path("user/register/", views.CreateUserView.as_view(), name="register"),
     path('profile/<int:pk>/', views.ProfileDetailView.as_view(), name='profile'),
-     path('swagger/', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
-    path('redoc/', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),
-]
+    url(r'^$', schema_view)
+   ]
