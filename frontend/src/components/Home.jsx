821d7704f1fbcb762cd0a1005a14f6d882540144
Ioannis Antoniou <johnantony24@hotmail.com>
2024-07-09 22:39:50 +0200

    login css

--- frontend/src/components/Home.jsx
+++ frontend/src/components/Home.jsx
@@ -26,6 +26,11 @@ const Home = () => {
           Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
         </p>
 
+        <p>
+          Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.
+        </p>
+        <button onClick=" ">Learn More</button>
+
       </section>
     </div>
     <div className="home-right">
