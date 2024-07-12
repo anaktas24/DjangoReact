38975c71f73795dd089ffa8dca308fdfff4b9ea0
Ioannis Antoniou <johnantony24@hotmail.com>
2024-07-12 21:48:16 +0200

    react scroll

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -2,15 +2,16 @@ import React from 'react';
 import '../styles/Navbar.css';
 import { Link } from 'react-router-dom';
 import vite from '../assets/vite.svg';
+import { FaHome, FaUser, FaInfoCircle } from 'react-icons/fa';
 
 const Navbar = () => {
   return (
     <nav className="navbar">
       <div className="navbar-logo" style = {{backgroundImage: `url(${vite})`}}></div>
       <ul className="navbar-links">
-        <li><Link to="/">Home</Link></li>
-        <li><Link to="/about">About</Link></li>
-        <li><Link to="/profile/">Profile</Link></li>
+        <li><Link to="/"><FaHome/></Link></li>
+        <li><Link to="/about"><FaInfoCircle/></Link></li>
+        <li><Link to="/profile/"><FaUser/></Link></li>
       </ul>
     </nav>
   );
