4411d88ea8cb708ad4248479cf78f0c956f68801
Ioannis Antoniou <johnantony24@hotmail.com>
2024-07-16 22:45:59 +0200

    Updated the login

--- frontend/src/components/Login.jsx
+++ frontend/src/components/Login.jsx
@@ -22,7 +22,7 @@ const Login = () => {
     setError('');  // Clear any previous errors
 
     try {
-      const response = await axios.post('http://127.0.0.1:8000/api/login/', credentials, {
+      const response = await axios.post('http://127.0.0.1:8000/api/user/login/', credentials, {
         headers: { 'Content-Type': 'application/json' }
       });
 
