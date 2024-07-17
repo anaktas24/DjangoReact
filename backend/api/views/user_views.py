0fe3a4cd782197c23bbb2d44cc121befa5dbbc84
Ioannis Antoniou <johnantony24@hotmail.com>
2024-07-17 22:07:57 +0200

    Updated the login

--- backend/api/views/user_views.py
+++ backend/api/views/user_views.py
@@ -35,23 +35,30 @@ class LoginUserView(generics.GenericAPIView):
     serializer_class = UserLoginSerializer
     permission_classes = [AllowAny]
     def post(self, request, *args, **kwargs):
-        logger.debug("Starting authentication process")
-        user = authenticate(username=request.data.get('email'), password=request.data.get('password'))
-        logger.debug(f"Authentication result for user: {user}")
+        logger.debug("Received login request with data: %s", request.data)
+        serializer = self.get_serializer(data=request.data)
 
-        if user is not None:
-            logger.debug("User authenticated, generating token")
-            # Your token generation logic here
-            try:
-                tokens = get_tokens_for_user(user)
-                return Response(tokens, status=status.HTTP_200_OK)
-            except Exception as e:
-                logger.error(f"Error in token generation: {str(e)}")
-                return Response({'error': 'Server error, please try again later.'}, status=status.HTTP_500_INTERNAL_SERVER_ERROR)
+        if serializer.is_valid():
+            email = serializer.validated_data['email']
+            password = serializer.validated_data['password']
+            logger.debug("Attempting to authenticate user %s", email)
+
+            user = authenticate(username=email, password=password)
+            if user:
+                logger.debug("User %s authenticated successfully", email)
+                # Assuming you have a function to get tokens for the user
+                try:
+                    tokens = get_tokens_for_user(user)
+                    return Response(tokens, status=status.HTTP_200_OK)
+                except Exception as e:
+                    logger.error("Failed to generate tokens: %s", str(e))
+                    return Response({'error': 'Failed to generate tokens'}, status=status.HTTP_500_INTERNAL_SERVER_ERROR)
+            else:
+                logger.info("Authentication failed for user %s", email)
+                return Response({'error': 'Invalid credentials'}, status=status.HTTP_401_UNAUTHORIZED)
         else:
-            logger.info("Authentication failed")
-            return Response({'error': 'Invalid credentials'}, status=status.HTTP_401_UNAUTHORIZED)
-
+            logger.error("Validation errors: %s", serializer.errors)
+            return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
     @staticmethod
     def get_tokens_for_user(user):  # Made static or could be outside of the class
