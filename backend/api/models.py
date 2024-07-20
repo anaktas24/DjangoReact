e39202906c63e83e70047509d810d14accddbb00
Ioannis Antoniou <johnantony24@hotmail.com>
2024-07-20 18:04:43 +0200

    restarting backend

--- backend/api/models.py
+++ backend/api/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from django.contrib.auth.models import AbstractUser
+from django.db.models.signals import post_save
 
 
 print("Loading User model...")
@@ -10,3 +11,25 @@ class User(AbstractUser):
 
     USERNAME_FIELD = "email"
     REQUIRED_FIELDS = ["username"]
+
+    def __str__(self):
+        return self.username
+
+
+class Profile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    full_name = models.CharField(max_length=200)
+    #image = models.ImageField(default = "default.jpg", upload_to="user_images")
+    bio = models.TextField(max_length=300)
+
+
+    def __str__(self):
+        return self.user.full_name
+
+
+def craete_user_profile(sender, instance, created, **kwargs):
+    if created:
+        Profile.objects.create(user=instance)
+
+def save_user_profile(sender, instance, **kwargs):
+    instance.profile.save()
