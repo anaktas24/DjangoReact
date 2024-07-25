809cb5b9d4229051097c4af168ecb01dc3e25398
Ioannis Antoniou <johnantony24@hotmail.com>
2024-07-25 15:08:02 +0200

    reset password

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,11 +1,14 @@
 import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom'
+import { ToastContainer } from 'react-toastify'
+import 'react-toastify/dist/ReactToastify.css'
 import Navbar from './components/Navbar'
 import Home from './components/Home'
-import Login from './components/Login'
-import Dashboard from './components/Dashboard'
-import Register from './components/Register'
-import NotFound from './components/NotFound'
+import Login from './pages/Login'
+import Dashboard from './pages/Dashboard'
+import Register from './pages/Register'
+import NotFound from './pages/NotFound'
 import Footer from './components/Footer'
+import ResetPassword from './pages/ResetPassword'
 
 import { AuthProvider } from './AuthContext'
 
@@ -27,11 +30,12 @@ function App() {
         <Routes>
           <Route path="/" element={<Home />} /> {/*we can put <ProtectedRoute>here so that we HAVE to login*/}
           <Route path="/login" element={<Login />} />
-          <Route path="/logout" element={<Logout />} />
+          <Route path="/reset-password" element={<ResetPassword />} />
           <Route path="/dashboard" element={<Dashboard />} />
           <Route path="/register" element={<Register />} />
           <Route path="*" element={<NotFound />} />
         </Routes>
+        <ToastContainer />
         <Footer />
       </AuthProvider>
     </BrowserRouter>
