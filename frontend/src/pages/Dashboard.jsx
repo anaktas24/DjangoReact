aaae7f26a56a8e7a607d1adafa3d07b37806c91a
Ioannis Antoniou <johnantony24@hotmail.com>
2024-08-05 22:52:12 +0200

    carousel test

--- frontend/src/pages/Dashboard.jsx
+++ frontend/src/pages/Dashboard.jsx
@@ -1,17 +1,56 @@
-import React from 'react'
-import { useSelector } from 'react-redux'
+import React from "react";
+
+import { NavLink, useNavigate } from "react-router-dom";
+import { useDispatch, useSelector } from "react-redux";
+import { logout, reset } from "../features/auth/authSlice";
+import { toast } from "react-toastify";
 
 
 const Dashboard = () => {
 
-    const { userInfo } = useSelector((state) => state.auth)
+  const navigate = useNavigate();
+  const dispatch = useDispatch();
 
+  const { user } = useSelector((state) => state.auth);
 
-    return (
-        <div>
-            <h1>Welcome, {userInfo.first_name} </h1>
+  const handleLogout = () => {
+    dispatch(logout());
+    dispatch(reset());
+    navigate("/");
+  };
+
+  return (
+    <nav className="nav">
+      <div className="container">
+        <NavLink className="logo" to="/">
+          Logo
+        </NavLink>
+        <div id="mainListDiv" className="main_list">
+          <ul className="navlinks">
+            {user ? (
+              <>
+                <NavLink className="navlinks" to="/profile">
+                  Profile
+                </NavLink>
+                <NavLink className="navlinks" to="/dashboard">
+                  Dashboard
+                </NavLink>
+                <NavLink className="navlinks" to="/" onClick={handleLogout}>
+                  Logout
+                </NavLink>
+              </>
+            ) : (
+              <>
+                <NavLink className="navlinks" to="/dashboard">
+                  Dashboard
+                </NavLink>
+              </>
+            )}
+          </ul>
         </div>
-    )
+      </div>
+    </nav>
+  );
 }
 
-export default Dashboard
+export default Dashboard;
