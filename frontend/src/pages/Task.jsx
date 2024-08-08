8f6da844f1bfd1ec9124feda938e625c0102b554
Ioannis Antoniou <johnantony24@hotmail.com>
2024-08-08 19:14:45 +0200

    Task page

---
+++ frontend/src/pages/Task.jsx
@@ -0,0 +1,30 @@
+import React from "react";
+import { useState } from "react";
+import '../styles/Task.css';
+
+
+
+
+
+
+
+function Task() {
+  const [isActiv, setIsActiv] = useState(false);
+
+
+
+
+  return (
+
+
+
+
+    <div>
+      <h1>Task</h1>
+
+      <p>Task page content</p>
+
+
+    </div>
+  );
+}
